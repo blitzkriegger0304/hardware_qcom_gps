184a224f45c21c2076040ca5700a2388d61fa113
Marc Babad <blitzkrieg.marc02@gmail.com>
2021-03-09 10:56:35 +0800

    gps: Silence 'out/target...unused parameter...' build warning
    Change-Id: I67d742c34dc7c7b09e805ad3f6165d363b717ce8
    Signed-off-by: Marc Babad <blitzkrieg.marc02@gmail.com>

--- msm8994/core/LocAdapterBase.h
+++ msm8994/core/LocAdapterBase.h
@@ -80,10 +80,10 @@ public:
 
     // This will be overridden by the individual adapters
     // if necessary.
-    inline virtual void setUlpProxy(UlpProxyBase* ulp) {}
+    inline virtual void setUlpProxy(UlpProxyBase* ulp __unused) {}
     virtual void handleEngineUpEvent();
     virtual void handleEngineDownEvent();
-    inline virtual void setPositionModeInt(LocPosMode& posMode) {}
+    inline virtual void setPositionModeInt(LocPosMode& posMode __unused) {}
     virtual void startFixInt() {}
     virtual void stopFixInt() {}
     virtual void getZppInt() {}
